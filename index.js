3596bea5d130c87fda2bbd76e41e782033b6c88e
Airam Trujillo Santos <airamtrs@gmail.com>
2019-06-17 20:38:24 +0100

    trying another js

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
+
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+    res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
+});
+
+app.listen(PORT, function () {
+    console.log('Server is running, port ' + PORT);
+});
