217f02f7d8b90a1d89eb4dee19ad4c006f4ef93e
Airam Trujillo Santos <airamtrs@gmail.com>
2019-06-18 20:39:03 +0100

    fixed scripts name

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ echo "REMOTE_PORT= ${REMOTE_PORT}"
 echo "REMOTE_USER= ${REMOTE_USER}"
 echo "REMOTE_HOST= ${REMOTE_HOST}"
 
-openssl aes-256-cbc -K $encrypted_3ee00ade31b5_key -iv $encrypted_3ee00ade31b5_iv -in deploy.enc -out ../sshfiles/deploy -d
+openssl aes-256-cbc -K $encrypted_3ee00ade31b5_key -iv $encrypted_3ee00ade31b5_iv -in deploy.enc -out deploy-travis -d
 eval "$(ssh-agent -s)"
 chmod 600 deploy-travis
 ssh-add deploy-travis
